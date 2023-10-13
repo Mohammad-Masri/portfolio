ff6023402b4cb1bd8ab9d05fc20ac84bbee6086a
Muhammad Al Masri <muhammadalmasri@MalishX-2.local>
2023-10-13 16:15:04 +0400

    ADD | links to about section

--- src/app/sections/about/components/about-title/index.tsx
+++ src/app/sections/about/components/about-title/index.tsx
@@ -1,9 +1,29 @@
+"use client";
 import DATA from "@/mock";
-import React from "react";
+import React, { ReactElement } from "react";
+import { Icon } from "@iconify/react";
+import Link from "next/link";
+
+type IconLinkProps = {
+  link: string;
+  icon: ReactElement;
+};
+
+function IconLink({ link, icon }: IconLinkProps) {
+  return (
+    <Link
+      href={link}
+      className=" border-[1px] border-[#242A41] rounded-full p-2"
+      target="_blank"
+    >
+      {icon}
+    </Link>
+  );
+}
 
 export default function AboutTitle() {
   return (
-    <div className="flex flex-col justify-center items-center">
+    <div className="flex flex-col justify-center items-center gap-10">
       <h1 className="text-[#1D2130] text-4xl text-left">
         HEY! <span className="font-bold">I'm {DATA.PROFILE.firstName},</span>
         <br />
@@ -11,6 +31,24 @@ export default function AboutTitle() {
           {DATA.PROFILE.position}
         </span>
       </h1>
+      <div className="flex flex-row gap-3">
+        <IconLink
+          link={DATA.PROFILE.linkedin}
+          icon={<Icon icon="mdi:linkedin" color="#242A41" width={30} />}
+        />
+        <IconLink
+          link={DATA.PROFILE.github}
+          icon={<Icon icon="ri:github-line" color="#242A41" width={30} />}
+        />
+        <IconLink
+          link={`https://wa.me/${DATA.PROFILE.whatsappNumber}`}
+          icon={<Icon icon="ic:baseline-whatsapp" color="#242A41" width={30} />}
+        />
+        <IconLink
+          link={`tel:${DATA.PROFILE.phoneNumber}`}
+          icon={<Icon icon="ic:baseline-phone" color="#242A41" width={30} />}
+        />
+      </div>
     </div>
   );
 }
