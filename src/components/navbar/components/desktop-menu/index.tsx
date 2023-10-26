bdc24dca511b82899f915ff0fecfab4b3fcb63dc
Muhammad Al Masri <muhammadalmasri@MalishX-2.local>
2023-10-26 10:29:27 +0400

    ADD | mobile menu, add the items

--- src/components/navbar/components/desktop-menu/index.tsx
+++ src/components/navbar/components/desktop-menu/index.tsx
@@ -1,44 +1,29 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import { Moods, navbarItems as NIs } from "@/constants";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { selectTheme } from "@/redux/slices/ThemeSlice";
-import NavbarItem, { NavbarItemProps } from "@/components/navbar-item";
+import NavbarItem from "@/components/navbar-item";
+import {
+  selectMenuItems,
+  setItemAsActive,
+} from "@/redux/slices/MenuItemsSlice";
 
 export default function DesktopMenu() {
   const theme = useSelector(selectTheme);
 
-  const [navbarItems, setNavbarItems] = useState<NavbarItemProps[]>(
-    NIs.map((i, index) => {
-      let isActive = false;
-      if (index == 0) isActive = true;
-      return {
-        id: i.id,
-        label: i.label,
-        link: `/#${i.id}`,
-        isActive,
-      };
-    })
-  );
+  const navbarItems = useSelector(selectMenuItems);
+  const dispatch = useDispatch();
 
-  const handleClickNavbarItem = (index: number) => {
-    const newNavbarItems = navbarItems.map((nbi, i) => {
-      if (i == index) nbi.isActive = true;
-      else nbi.isActive = false;
-      return nbi;
-    });
-
-    setNavbarItems(newNavbarItems);
+  const handleClickNavbarItem = (id: string) => {
+    dispatch(setItemAsActive(id));
   };
   return (
     <ul className="hidden lg:flex flex-row gap-4">
       {navbarItems.map((item, index) => (
-        <div key={index} onClick={() => handleClickNavbarItem(index)}>
+        <div key={index} onClick={() => handleClickNavbarItem(item.id)}>
           <NavbarItem
-            id={item.id}
-            label={item.label}
-            link={item.link}
-            isActive={item.isActive}
+            menuItem={item}
             textColor={theme == Moods.Light ? "white" : "#43495B"}
             inactiveBackgroundColor={theme == Moods.Light ? "#1D2130" : "white"}
           />
